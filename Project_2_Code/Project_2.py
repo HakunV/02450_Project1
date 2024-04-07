86bef69c4c65f07430e1431d7b3f1bb81680c407
HakunV <hakun_vestergaard@hotmail.com>
2024-04-07 02:32:11 +0200

    Progress Part A

--- Project_2_Code/Project_2.py
+++ Project_2_Code/Project_2.py
@@ -7,7 +7,10 @@ from scipy.stats import zscore
 import pandas as pd
 import sklearn.linear_model as lm
 
-from matplotlib.pyplot import figure, legend, plot, show, title, xlabel, ylabel, axis, grid, bar, xticks, table, hist, ylim, subplot, boxplot, scatter
+from sklearn import model_selection
+from dtuimldmtools import rlr_validate
+
+from matplotlib.pyplot import figure, legend, plot, show, title, xlabel, ylabel, axis, grid, bar, xticks, table, hist, ylim, subplot, boxplot, scatter, loglog, semilogx
 
 filename = importlib_resources.files("dtuimldmtools").joinpath("data/glass.data")
 
@@ -38,14 +41,18 @@ C = len(classNames)
 
 # Regression Part a: Linear Regression, trying to predict the refractive index of the glass
 
-X_stand = X - np.ones((N, 1)) * X.mean(axis=0)
-X_stand = X_stand * (1 / np.std(X_stand, 0))
+# X_stand = X - np.ones((N, 1)) * X.mean(axis=0)
+# X_stand = X_stand * (1 / np.std(X_stand, 0))
+X_stand = X
 
 refractive_idx = attributeNames.index("Ri")
 y_reg = X_stand[:, refractive_idx]
 
 X_cols = list(range(0, refractive_idx)) + list(range(refractive_idx+1, len(attributeNames)))
 X_stand = X_stand[:, X_cols]
+
+attributeNames = attributeNames[1:9]
+M = M - 1
 
 model = lm.LinearRegression()
 model.fit(X_stand, y_reg)
@@ -60,5 +67,142 @@ xlabel("Refractive index (true)")
 ylabel("Refractive index (estimated)")
 subplot(2, 1, 2)
 hist(residual, 40)
+
+# Add offset attribute
+X_stand_off = np.concatenate((np.ones((X_stand.shape[0], 1)), X_stand), 1)
+attributeNames = ["Offset"] + attributeNames
+M = M + 1
+
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+K = 10
+CV = model_selection.KFold(K, shuffle=True)
+# CV = model_selection.KFold(K, shuffle=False)
+
+# Values of lambda
+lambdas = np.power(10.0, range(-5, 9))
+
+# Initialize variables
+# T = len(lambdas)
+Error_train = np.empty((K, 1))
+Error_test = np.empty((K, 1))
+Error_train_rlr = np.empty((K, 1))
+Error_test_rlr = np.empty((K, 1))
+Error_train_nofeatures = np.empty((K, 1))
+Error_test_nofeatures = np.empty((K, 1))
+w_rlr = np.empty((M, K))
+w_noreg = np.empty((M, K))
+
+k = 0
+for train_index, test_index in CV.split(X_stand_off, y_reg):
+
+    X_train = X_stand_off[train_index]
+    y_train = y_reg[train_index]
+    X_test = X_stand_off[test_index]
+    y_test = y_reg[test_index]
+    
+    (
+        opt_val_err,
+        opt_lambda,
+        mean_w_vs_lambda,
+        train_err_vs_lambda,
+        test_err_vs_lambda,
+    ) = rlr_validate(X_train, y_train, lambdas)
+
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+
+    # Compute mean squared error without using the input data at all
+    Error_train_nofeatures[k] = (
+        np.square(y_train - y_train.mean()).sum(axis=0) / y_train.shape[0]
+    )
+    Error_test_nofeatures[k] = (
+        np.square(y_test - y_test.mean()).sum(axis=0) / y_test.shape[0]
+    )
+
+    # Estimate weights for the optimal value of lambda, on entire training set
+    lambdaI = opt_lambda * np.eye(M)
+    lambdaI[0, 0] = 0  # Do no regularize the bias term
+    w_rlr[:, k] = np.linalg.solve(XtX + lambdaI, Xty).squeeze()
+    # Compute mean squared error with regularization with optimal lambda
+    Error_train_rlr[k] = (
+        np.square(y_train - X_train @ w_rlr[:, k]).sum(axis=0) / y_train.shape[0]
+    )
+    Error_test_rlr[k] = (
+        np.square(y_test - X_test @ w_rlr[:, k]).sum(axis=0) / y_test.shape[0]
+    )
+    
+    # Estimate weights for unregularized linear regression, on entire training set
+    w_noreg[:, k] = np.linalg.solve(XtX, Xty).squeeze()
+    # Compute mean squared error without regularization
+    Error_train[k] = (
+        np.square(y_train - X_train @ w_noreg[:, k]).sum(axis=0) / y_train.shape[0]
+    )
+    Error_test[k] = (
+        np.square(y_test - X_test @ w_noreg[:, k]).sum(axis=0) / y_test.shape[0]
+    )
+    
+    if k == K - 1:
+        figure(k, figsize=(12, 8))
+        subplot(1, 2, 1)
+        semilogx(lambdas, mean_w_vs_lambda.T[:, 1:], ".-")  # Don't plot the bias term
+        xlabel("Regularization factor")
+        ylabel("Mean Coefficient Values")
+        grid()
+        # You can choose to display the legend, but it's omitted for a cleaner
+        # plot, since there are many attributes
+        # legend(attributeNames[1:], loc='best')
+
+        subplot(1, 2, 2)
+        title("Optimal lambda: 1e{0}".format(np.log10(opt_lambda)))
+        loglog(
+            lambdas, train_err_vs_lambda.T, "b.-", lambdas, test_err_vs_lambda.T, "r.-"
+        )
+        xlabel("Regularization factor")
+        ylabel("Squared error (crossvalidation)")
+        legend(["Train error", "Validation error"])
+        grid()
+        
+    # To inspect the used indices, use these print statements
+    print('Cross validation fold {0}/{1}:'.format(k+1,K))
+    print('Train indices: {0}'.format(train_index))
+    print('Test indices: {0}\n'.format(test_index))
+
+    k += 1
+
+# Display results
+print("Linear regression without feature selection:")
+print("- Training error: {0}".format(Error_train.mean()))
+print("- Test error:     {0}".format(Error_test.mean()))
+print(
+    "- R^2 train:     {0}".format(
+        (Error_train_nofeatures.sum() - Error_train.sum())
+        / Error_train_nofeatures.sum()
+    )
+)
+print(
+    "- R^2 test:     {0}\n".format(
+        (Error_test_nofeatures.sum() - Error_test.sum()) / Error_test_nofeatures.sum()
+    )
+)
+print("Regularized linear regression:")
+print("- Training error: {0}".format(Error_train_rlr.mean()))
+print("- Test error:     {0}".format(Error_test_rlr.mean()))
+print(
+    "- R^2 train:     {0}".format(
+        (Error_train_nofeatures.sum() - Error_train_rlr.sum())
+        / Error_train_nofeatures.sum()
+    )
+)
+print(
+    "- R^2 test:     {0}\n".format(
+        (Error_test_nofeatures.sum() - Error_test_rlr.sum())
+        / Error_test_nofeatures.sum()
+    )
+)
+
+print("Weights in last fold:")
+for m in range(M):
+    print("{:>15} {:>15}".format(attributeNames[m], np.round(w_rlr[m, -1], 5)))
 
 show()
